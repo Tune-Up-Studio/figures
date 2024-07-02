2fb191a72462fefd912cbaca6dc4266754fb9255
hojooneum <bldolphin96@gmail.com>
2024-07-03 00:23:06 +0900

    feat(no-declaration): generate no declaration when compiled

--- dist/definitions/Curve.js
+++
@@ -1,9 +0,0 @@
-export var CurveLevel;
-(function (CurveLevel) {
-    CurveLevel["zero"] = "Slighter";
-    CurveLevel["one"] = "Slight";
-    CurveLevel["two"] = "Lighter";
-    CurveLevel["three"] = "Tight";
-    CurveLevel["four"] = "Tighter";
-    CurveLevel["five"] = "Full";
-})(CurveLevel || (CurveLevel = {}));
--- dist/definitions/Thickness.js
+++
@@ -1,8 +0,0 @@
-export var ThicknessLevel;
-(function (ThicknessLevel) {
-    ThicknessLevel["zero"] = "Thiner";
-    ThicknessLevel["one"] = "Thin";
-    ThicknessLevel["two"] = "Mid";
-    ThicknessLevel["four"] = "Thick";
-    ThicknessLevel["five"] = "Thicker";
-})(ThicknessLevel || (ThicknessLevel = {}));
